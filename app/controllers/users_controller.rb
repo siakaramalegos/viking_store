73848af47720b6e78e1dfbdc4f9051e38d8d6f0f
Sia <siakaramalegos@gmail.com>
2016-01-21 18:21:46 -0600

    Fixes default billing and shipping id setting in user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      set_default_addresses
       sign_in(@user)
       redirect_to products_index_url, notice: "You successfully registered!"
     else
@@ -25,12 +26,13 @@ class UsersController < ApplicationController
   def edit
     @user = User.where(id: current_user.id).includes(:addresses => :city).first
     addresses = @user.addresses
-    addresses.build
-    addresses.last.build_city
+    # addresses.build
+    # addresses.last.build_city
   end
 
   def update
     if @user.update(user_params)
+      set_default_addresses
       redirect_to products_index_url, notice: "Your user information was successfully updated!"
     else
       flash.now[:alert] = "There was a problem submitting your form.  See below for errors."
@@ -49,11 +51,35 @@ class UsersController < ApplicationController
 
   private
 
+  def set_default_addresses
+    if addresses = user_params[:addresses_attributes]
+      if billing_index = params[:default_billing_index]
+        address = addresses[billing_index]
+        if address[:id]
+          @user.billing_id = address[:id]
+        else
+          @user.billing_id = @user.addresses.where(street_address: address[:street_address]).first.id
+        end
+      end
+
+      if shipping_index = params[:default_shipping_index]
+        address = addresses[shipping_index]
+        if address[:id]
+          @user.shipping_id = address[:id]
+        else
+          @user.shipping_id = @user.addresses.where(street_address: address[:street_address]).first.id
+        end
+      end
+
+      @user.save
+    end
+  end
+
   def set_user
     @user = current_user
   end
 
   def user_params
-    params.require(:user).permit(:email, :first_name, :last_name, :phone_number, :billing_id, :shipping_id, { :addresses_attributes => [:id, :street_address, :secondary_address, :state_id, :zip_code, :_destroy, city_attributes: [:name]] })
+    params.require(:user).permit(:email, :first_name, :last_name, :phone_number, { :addresses_attributes => [:id, :street_address, :secondary_address, :state_id, :zip_code, :_destroy, city_attributes: [:name]] })
   end
 end
